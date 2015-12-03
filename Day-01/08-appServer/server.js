a75b5e68058f69a14d197f200825b2a35948e06c
tkmagesh <tkmagesh77@gmail.com>
2015-12-03 16:45:17 +0530

    Day-1 Complete

--- Day-01/08-appServer/server.js
+++ Day-01/08-appServer/server.js
@@ -1,17 +1,17 @@
-
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var url = require('url');
+var qs = require('querystring');
 var calculator = require('./calculator');
 
 /*
-if (req -> static resource, serve it accordingly)
-if (req -> /calculator, extract data from url , process and return the result)
+    - parsing req data
+    - serving static resources
+    - serving /calculator requests
+    - serving 404
 
-/calculator?n1=100&n2=200&operation=add
 */
-
 var staticExtns = ['.html','.js','.css','.xml','.json'];
 function isStatic(resourcePath){
     var extn = path.extname(resourcePath);
@@ -21,7 +21,6 @@ function isStatic(resourcePath){
 var server = http.createServer(function(req, res){
     console.log('a new connection is established for ', req.url);
     var urlObj = url.parse(req.url, true);
-    console.log(urlObj.pathname);
     if (isStatic(urlObj.pathname)){
         var resource = path.join(__dirname, urlObj.pathname);
         if (fs.existsSync(resource)){
@@ -30,7 +29,7 @@ var server = http.createServer(function(req, res){
             res.statusCode = 404;
             res.end();
         }
-    } else if (urlObj.pathname === '/calculator'){
+    } else if (urlObj.pathname === '/calculator' && req.method === 'GET'){
         var n1 = parseInt(urlObj.query.n1, 10),
             n2 = parseInt(urlObj.query.n2, 10),
             operation = urlObj.query.operation;
@@ -38,6 +37,20 @@ var server = http.createServer(function(req, res){
         res.write(result.toString());
         res.end();
 
+    } else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+        var reqDataString = '';
+        req.on('data', function(chunk){
+            reqDataString += chunk;
+        });
+        req.on('end', function(){
+            var reqData = qs.parse(reqDataString);
+            var n1 = parseInt(reqData.n1, 10),
+                n2 = parseInt(reqData.n2, 10),
+                operation = reqData.operation;
+            var result = calculator[operation](n1,n2);
+            res.write(result.toString());
+            res.end();
+        });
     } else {
         res.statusCode = 404;
         res.end();
