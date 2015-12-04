dcedaa6438dea2c6bbff40ae91a0d20642810187
tkmagesh <tkmagesh77@gmail.com>
2015-12-04 14:21:54 +0530

    Express - complete

--- Day-02/11-taskManager/routes/taskRoutes.js
+++ Day-02/11-taskManager/routes/taskRoutes.js
@@ -13,6 +13,34 @@ router.get('/', function(req, res, next) {
     res.render('tasks/index', viewData);
 });
 
+router.get('/new', function(req, res, next){
+    res.render('tasks/new');
+});
+
+router.post('/new', function(req, res, next){
+    var taskName = req.body.txtTask;
+    var newId = tasks.reduce(function(result, task){
+        return result > task.id ? result : task.id;
+    },0) + 1;
+    var newTask = {
+        id : newId,
+        name : taskName,
+        isCompleted : false
+    };
+    tasks.push(newTask);
+    res.redirect('/tasks');
+});
+
+router.get('/toggle/:id', function(req, res, next){
+    var id = parseInt(req.params.id);
+    var task = tasks.filter(function(t){
+        return t.id === id;
+    })[0];
+    if (task)
+        task.isCompleted = !task.isCompleted;
+    res.redirect('/tasks');
+});
+
 module.exports = router;
 
 
